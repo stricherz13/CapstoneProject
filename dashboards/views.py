26734b72f9295f1e6c97f1919263c88eb20fda7f
stricherz13 <94708588+stricherz13@users.noreply.github.com>
2023-05-03 18:55:15 -0500

    Added LRA and Sale 216 Dashboards

--- dashboards/views.py
+++ dashboards/views.py
@@ -8,7 +8,7 @@ class StLouisLandTaxDashboard2022(TemplateView):
     template_name = 'dashboards/2022StLouisLandTaxDashboard.html'
 
 class StLouisLandTaxDashboard2023(TemplateView):
-    template_name = 'dashboards/StLouisLandTaxDashboard2023.html'
+    template_name = 'dashboards/2023StLouisLandTaxDashboard.html'
 
 class LRADashboard(TemplateView):
     template_name = 'dashboards/LRADashboard.html'
