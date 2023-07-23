8be5829d3895614649fbc8bc8f9b5fc0add51174
stricherz13 <94708588+stricherz13@users.noreply.github.com>
2023-07-23 09:45:55 -0500

    Updated for St Charles Tax Sale

--- maps/urls.py
+++ maps/urls.py
@@ -6,5 +6,6 @@ app_name = 'maps'
 urlpatterns = [
     path("2022StLouisLandTaxMap", views.combinedView, name='StLouisCityLandTaxSale'),
     path("2023StLouisLandTaxMap", views.combinedViewSTL2023, name='StLouisCityLandTaxSale2023'),
-    path("LRAMap", views.LRAcombinedView, name='LRA')
+    path("LRAMap", views.LRAcombinedView, name='LRA'),
+    path("2023StCharlesTaxMap", views.StCharles2023combinedView, name='StCharles2023')
 ]
